619bf440ed0c7bc85cacf98ea690d89fba11ad22
Jakub Szamotulski <jmszamotulski@gmail.com>
2022-12-06 20:01:58 +0000

    Part two code

--- main.py
+++ main.py
@@ -9,4 +9,16 @@ for line in file:
         calorie.append(calorieCounter)
         calorieCounter = 0
 
+# Highest calorie value
 print(max(calorie))
+
+# Top 3 calorie values added together
+top1 = calorie.index(max(calorie))
+top1val = max(calorie)
+calorie.pop(top1)
+top1 = calorie.index(max(calorie))
+top2val = max(calorie)
+calorie.pop(top1)
+top1 = calorie.index(max(calorie))
+top3val = max(calorie)
+print(top1val+top2val+top3val)
